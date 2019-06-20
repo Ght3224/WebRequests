305e695d812763134fadb7bef7df5320dd28b3ee
YOUR NAME <adien330@yahoo.com>
2019-06-20 19:52:50 -0400

    show art in TN city

---
+++ art.rb
@@ -0,0 +1,16 @@
+require "http"
+require "ffaker"
+
+
+
+response = HTTP.get("https://data.nashville.gov/resource/dqkw-tj5j.json")
+
+
+all_the_art = response.parse
+
+all_the_art.each do |art|
+  p art ["title"]
+end 
+
+
+Ffaker::
