2d0dccb56684743201a72f4c0fc6f1d1be829d2b
YOUR NAME <adien330@yahoo.com>
2019-06-20 22:22:30 -0400

    Almost perfect running program as per instructions

--- exercise.rb
+++ exercise.rb
@@ -1,51 +1,59 @@
 require "http"   #allows to make web requests from ruby to internet 
 
 
-p "word?!?"
-user_input = gets.chomp
-
-response = HTTP.get("https://api.wordnik.com/v4/word.json/#{user_input}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=")
+while true 
 
 
+  p "word?!?"
+  user_input = gets.chomp
 
-body_of = response.parse
+   if user_input == "quit"
+    break 
+  end 
 
-body_of.each do |hash|
-  p hash["text"]
+  response = HTTP.get("https://api.wordnik.com/v4/word.json/#{user_input}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=8")
+
+
+
+  body_of = response.parse
+
+  body_of.each do |hash|
+    p hash["text"]
+    break
+  end 
+
+
+
+
+  p "________________________________________________"
+
+  response2 = HTTP.get("https://api.wordnik.com/v4/word.json/#{user_input}/topExample?useCanonical=false&api_key=")
+
+
+
+
+  body_of_2 = response2.parse 
+
+
+  p body_of_2["text"]
+
+
+
+  p "Pronunciations"
+  p "______________________________________________________"
+  response3 = HTTP.get("https://api.wordnik.com/v4/word.json/hippo/pronunciations?useCanonical=false&limit=50&api_key=")
+
+
+  body_of_3 = response3.parse 
+
+
+  body_of_3.each do |hash|
+    p hash["raw"]
+  end 
+
+
+
 end 
-
-
-
-
-p "________________________________________________"
-
-response2 = HTTP.get("https://api.wordnik.com/v4/word.json/#{user_input}/topExample?useCanonical=false&api_key=")
-
-
-
-
-body_of_2 = response2.parse 
-
-
-p body_of_2["text"]
-
-
-
-p "Pronunciations"
-p "________________________________________________________"
-response3 = HTTP.get("https://api.wordnik.com/v4/word.json/hippo/pronunciations?useCanonical=false&limit=50&api_key=)
-
-
-body_of_3 = response3.parse 
-
-
-body_of_3.each do |hash|
-  p hash["raw"]
-end 
-
-
-
-
 
 
 
